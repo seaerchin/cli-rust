4b454e57df794bbb2ec8bfcd43e7a5ebb790072b
seaerchin <jiachin@open.gov.sg>
2022-02-20 14:53:51 +0800

    refactor(lib): add impl

--- src/catr/src/lib.rs
+++ src/catr/src/lib.rs
@@ -1,7 +1,7 @@
 use std::{
     error::Error,
     fs,
-    io::{self, BufReader},
+    io::{self, BufRead, BufReader},
 };
 
 use clap::{App, Arg};
@@ -19,15 +19,12 @@ pub struct Config {
 // 2: Check if should number_lines || number_non_blank
 // 3: Pretty print
 pub fn run(config: &Config) -> Res<()> {
-    let contents: Vec<Result<String, &str>> =
-        config.files.iter().map(|filepath| open(filepath)).collect();
+    let contents: Vec<_> = config.files.iter().map(|filepath| open(filepath)).collect();
 
     for res in contents {
         match res {
             Ok(contents) => {
-                let output =
-                    get_output(contents, config.number_lines, config.number_nonblank_lines);
-                print!("{}", output)
+                print_output(contents, config.number_lines, config.number_nonblank_lines)
             }
             Err(e) => eprintln!("catr: {e} - No such file or directory"),
         }
@@ -37,40 +34,33 @@ pub fn run(config: &Config) -> Res<()> {
 
 // If we read successfully, the caller should own the result.
 // Otherwise, we can return the path as is.
-fn open(path: &str) -> Result<String, &str> {
+fn open(path: &str) -> Result<Box<dyn BufRead>, &str> {
     if path == "" || path == "-" {
-        let reader = Ok::<_, i32>(BufReader::new(io::stdin()));
-        todo!()
+        return Ok(Box::new(BufReader::new(io::stdin())));
     }
-    fs::read_to_string(path).map_err(|_| path)
+
+    let file_result = fs::File::open(path);
+    match file_result {
+        Ok(file) => Ok(Box::new(BufReader::new(file))),
+        Err(_) => Err(path),
+    }
 }
 
-// 1: Split by new lines
-// 2: Check if we only need the nonempty lines
-// 3: Zip with input then map to the output string
-fn get_output(contents: String, number_lines: bool, number_nonblank: bool) -> String {
-    contents
-        .split("\n")
-        .filter(|line| {
-            if number_nonblank {
-                !line.is_empty()
+fn print_output<T: BufRead>(reader: T, number_lines: bool, number_nonblank: bool) {
+    let mut idx = 0;
+    for line_result in reader.lines() {
+        if let Ok(line) = line_result {
+            // check if we need to number
+            if number_nonblank && !line.is_empty() {
+                println!("{:>6}\t{}", idx, line);
+                idx += 1;
+            } else if number_lines {
+                println!("{:>6}\t{}", idx, line);
             } else {
-                true
+                println!("{}", line)
             }
-        })
-        .zip(0..)
-        .map(|(line, line_no)| {
-            format!(
-                "{}{}\n",
-                if number_lines || number_nonblank {
-                    format!("{} ", line_no.to_string())
-                } else {
-                    String::from("")
-                },
-                line
-            )
-        })
-        .collect()
+        }
+    }
 }
 
 pub fn get_args() -> Res<Config> {
