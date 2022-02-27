dc76ce27a999aed55df3d8c6e3347d2a70ac995b
seaerchin <jiachin@open.gov.sg>
2022-02-28 02:28:07 +0800

    refactor(lib): add more code

--- src/findr/src/lib.rs
+++ src/findr/src/lib.rs
@@ -20,6 +20,10 @@ pub struct Config {
     entry_types: Vec<EntryType>,
 }
 
+fn is_match(regs: &Vec<Regex>, s: &str) -> bool {
+    regs.len() == 0 || regs.iter().any(|r| r.is_match(s))
+}
+
 pub fn get_args() -> Res<Config> {
     let matches = App::new("findr")
         .version("0.1.0")
@@ -39,8 +43,7 @@ pub fn get_args() -> Res<Config> {
                 .short("t")
                 .long("type")
                 .value_name("TYPE")
-                .possible_values(&["d", "l", "f"])
-                .default_value("f"),
+                .possible_values(&["d", "l", "f"]),
         )
         .get_matches();
 
@@ -55,7 +58,7 @@ pub fn get_args() -> Res<Config> {
     // NOTE: This is defaulted to file
     let entry_types = matches
         .values_of_lossy("type")
-        .unwrap()
+        .unwrap_or(vec!["d".to_string(), "l".to_string(), "f".to_string()])
         .iter()
         .map(|t| {
             let s: &str = t;
@@ -80,7 +83,27 @@ pub fn run(config: Config) -> Res<()> {
         for entry in WalkDir::new(path) {
             match entry {
                 Err(e) => eprintln!("{}", e),
-                Ok(entry) => println!("{}", entry.path().display()),
+                Ok(entry) => {
+                    let filetype = entry.file_type();
+                    let filename = entry.file_name().to_string_lossy();
+                    if filetype.is_dir() && config.entry_types.contains(&Dir) {
+                        if is_match(&config.names, &filename) {
+                            println!("{}", entry.path().display())
+                        }
+                    }
+
+                    if filetype.is_symlink() && config.entry_types.contains(&Link) {
+                        if is_match(&config.names, &filename) {
+                            println!("{}", entry.path().display())
+                        }
+                    }
+
+                    if filetype.is_file() && config.entry_types.contains(&File) {
+                        if is_match(&config.names, &filename) {
+                            println!("{}", entry.path().display())
+                        }
+                    }
+                }
             }
         }
     }
